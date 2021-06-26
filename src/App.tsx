5190a65db076540e1681eed88e6edfd6cbc4b962
rafaelrolivares <rafaelrolivares@gmail.com>
2021-06-26 10:16:31 +0200

    installs dependencies and sets map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+// react
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+// openlayers
+import TileLayer from 'ol/layer/Tile';
+import Map from 'ol/Map';
+import 'ol/ol.css';
+import { fromLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+
+import { ActionComponent } from './components/ActionComponent';
+
 function App() {
+  const [map, setMap] = useState<any | null>(null);
+
+  const mapElement = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mapElement.current) {
+      const initialMap = new Map({
+        target: mapElement.current,
+        layers: [new TileLayer({ source: new OSM() })],
+        view: new View({
+          center: fromLonLat([-58.43, -34.63]),
+          zoom: 12,
+          maxZoom: 20,
+        }),
+      });
+
+      setMap(initialMap);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapElement} className="ol-map"></div>
+      {/* <ActionComponent map={map} /> */}
     </div>
   );
 }
---
+++ src/components/ActionComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from 'ol/Map';
+
+type ActionComponentProps = {
+  map: Map;
+};
+
+export const ActionComponent = ({ map }: ActionComponentProps) => {
+  return <div>ooioioi</div>;
+};
