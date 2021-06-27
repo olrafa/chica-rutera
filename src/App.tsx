349e94b55450397b83e7296781228c6cdb3b087c
rafaelrolivares <rafaelrolivares@gmail.com>
2021-06-27 12:44:39 +0200

    adds style to layers

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import OSM from 'ol/source/OSM';
 import View from 'ol/View';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
+import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
 
 // css
 import 'ol/ol.css';
@@ -25,7 +26,21 @@ function App() {
   useEffect(() => {
     if (mapElement.current) {
       const source = new VectorSource();
-      const vector = new VectorLayer({ source });
+
+      const vector = new VectorLayer({
+        source,
+        style: new Style({
+          image: new CircleStyle({
+            radius: 7,
+            fill: new Fill({ color: '#0AA' }),
+            stroke: new Stroke({
+              color: '#258',
+              width: 2,
+            }),
+          }),
+        }),
+        zIndex: 10
+      });
       const initialMap = new Map({
         target: mapElement.current,
         layers: [new TileLayer({ source: new OSM() }), vector],
--- src/components/ShowRoute.tsx
+++ src/components/ShowRoute.tsx
@@ -5,12 +5,12 @@ import Polyline from 'ol/format/Polyline';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
 import Geometry from 'ol/geom/Geometry';
+import { Stroke, Style } from 'ol/style';
 
 export const ShowRoute = ({ routing, map }: any) => {
   const { routes } = routing;
 
   const routeLines = routes.map(({ geometry }: any) => {
-
     const trace = new Polyline().readGeometry(geometry, {
       dataProjection: 'EPSG:4326',
       featureProjection: 'EPSG:3857',
@@ -23,16 +23,24 @@ export const ShowRoute = ({ routing, map }: any) => {
   });
 
   const source = new VectorSource();
-  const vector = new VectorLayer({ source });
+  const vector = new VectorLayer({
+    source,
+    style: new Style({
+      stroke: new Stroke({
+        width: 6,
+        color: '#5AF',
+      }),
+    }),
+  });
 
   routeLines.forEach((line: Feature<Geometry>) => {
-    console.log(line.getGeometry());
     source.addFeature(line);
   });
 
   map.addLayer(vector);
+  map.getView().fit(source.getExtent(), { padding: [50, 50, 50, 350] });
 
   console.log(source.getFeatures());
 
-  return <div>ruta</div>;
+  return <div>Route showing on map</div>;
 };
