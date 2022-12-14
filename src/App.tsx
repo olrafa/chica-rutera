8938ca41f50c537de6c52e814183459bcb9203eb
Rafael Olivares <rafaelrolivares@gmail.com>
2022-12-14 15:51:22 -0300

    adds react-query

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import "./App.css";
 
 import React from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import MapComponent from "./components/Map";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="App">
-      <MapComponent />
+      <QueryClientProvider client={queryClient}>
+        <MapComponent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   );
 }
---
+++ src/components/Map/useGetUserUserIpInfo.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { GEOAPIFY_IP_INFO } from "../../requests/geoapify/constants";
+import convertTimeToMilliseconds from "../../util/convertTimeToMilliseconds";
+
+const useGetUserIpInfo = () =>
+  useQuery(["ipInfo"], getIpInfo, {
+    onError: (error) => console.log("Error getting IP Info", error),
+    staleTime: convertTimeToMilliseconds(2, "hours")
+  });
+
+// Not really all there is to it but what we're using so far
+type IpInfoResponse = {
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
+/**
+ * Gets the user's IP info to zoom the map to their area when opening the app.
+ * @returns the result of the query
+ */
+const getIpInfo = async (): Promise<IpInfoResponse> => {
+  const data = await fetch(GEOAPIFY_IP_INFO, { method: "GET" });
+  const response = await data.json();
+  return response;
+};
+
+export default useGetUserIpInfo;
