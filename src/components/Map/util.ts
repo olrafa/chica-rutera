00c3dec22646c91e7851d05b005457b52c64d789
Rafael Olivares <rafaelrolivares@gmail.com>
2022-12-15 15:48:30 -0300

    test

--- src/components/Map/util.ts
+++ src/components/Map/util.ts
@@ -15,12 +15,12 @@ const createPoint = (coordinate: Coordinate, formatted?: string) =>
 
 const getCoordinates = (lon: string, lat: string): Coordinate => {
   const lonLat = [lon, lat].map((c) => parseFloat(c));
-  return fromLonLat(lonLat)
+  return fromLonLat(lonLat);
 };
 
 export const createRoutePoint = ({ formatted, lon, lat }: any) => {
-  const coordinate = getCoordinates(lon, lat)
-  return createPoint(coordinate, formatted)
+  const coordinate = getCoordinates(lon, lat);
+  return createPoint(coordinate, formatted);
 };
 
 export const createStyle = (color: string) =>
