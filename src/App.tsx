9db6d7987ab538473078fbbf126d8974be025e92
Rafael Olivares <rafaelrolivares@gmail.com>
2022-12-27 11:54:08 -0300

    minimize requests while hot reloading

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import "./App.css";
 
 import React from "react";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 import MapComponent from "./components/Map";
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        cacheTime: 1000 * 60 * 60 * 8, // 8 hours
+      },
+    },
+  });
+
+  const persister = createSyncStoragePersister({
+    storage: window.localStorage,
+  });
 
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
         <MapComponent />
         <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
+      </PersistQueryClientProvider>
     </div>
   );
 }
