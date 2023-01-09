845a0c9866b79dfabbcfd234d2d4c889bed23e85
Rafael Olivares <rafaelrolivares@gmail.com>
2023-01-09 12:14:23 -0300

    inputs

--- src/components/Form/RouteInputs.tsx
+++ src/components/Form/RouteInputs.tsx
@@ -3,9 +3,7 @@ import { ReactElement, useContext } from "react";
 import { DestinationType, RoutePoint } from "../../types/route.types";
 import MapContext from "../MapComponent/MapContext";
 
-import searchForAddress from "./searchAndAddPoint";
-
-const PLACEHOLDER = "Search for an address and press 'Enter'";
+import InputField from "./InputField";
 
 type RouteInputsProps = {
   start: RoutePoint | undefined;
@@ -18,29 +16,11 @@ const RouteInputs = ({
   end,
   updateRoute,
 }: RouteInputsProps): ReactElement => {
-  const { map, startLayer, endLayer, stopsLayer } = useContext(MapContext);
-  const startValue = start?.get("name");
-  const endValue = end?.get("name");
-  console.log(start, end);
+  const { endLayer, stopsLayer } = useContext(MapContext);
+  const startValue = start?.get("name") || "";
+  const endValue = end?.get("name") || "";
 
-  const destinationLayers = {
-    start: startLayer,
-    end: endLayer,
-    stops: stopsLayer,
-  };
-
-  const handleAddressInput = (
-    e: { key: string; target: any },
-    item: DestinationType
-  ) =>
-    e.key === "Enter" &&
-    searchForAddress(
-      e.target.value,
-      item,
-      map,
-      destinationLayers[item],
-      updateRoute
-    );
+  const stops = stopsLayer.getFeatures();
 
   const copyEndFromStart = () => {
     if (start) {
@@ -52,24 +32,18 @@ const RouteInputs = ({
 
   return (
     <div>
-      <div className="search-item">
-        <label htmlFor="search-start">Starting point:</label>
-        <input
-          id="search-start"
-          type="text"
-          onKeyDown={(e) => handleAddressInput(e, "start")}
-          placeholder={PLACEHOLDER}
-          defaultValue={startValue}
-        />
-      </div>
-      <div className="search-item">
-        <label htmlFor="search-end">Ending point:</label>
-        <input
-          id="search-end"
-          type="text"
-          onKeyDown={(e) => handleAddressInput(e, "end")}
-          placeholder={PLACEHOLDER}
-          defaultValue={endValue}
+      <InputField
+        label="Starting point"
+        destination="start"
+        callback={updateRoute}
+        value={startValue}
+      />
+      <div>
+        <InputField
+          label="Ending point"
+          destination="end"
+          callback={updateRoute}
+          value={endValue}
         />
         {start && (
           <span className="repeat-start-btn" onClick={copyEndFromStart}>
@@ -77,15 +51,12 @@ const RouteInputs = ({
           </span>
         )}
       </div>
-      <div className="search-item">
-        <label htmlFor="search-stops">Add stops:</label>
-        <input
-          id="search-stops"
-          type="text"
-          onKeyDown={(e) => handleAddressInput(e, "stops")}
-          placeholder={PLACEHOLDER}
-        />
-      </div>
+      <InputField
+        label="Add stops"
+        destination="stops"
+        callback={updateRoute}
+        stops={stops}
+      />
     </div>
   );
 };
