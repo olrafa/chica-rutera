c3cbef1249cf497cbd1fd8a655e5e2142d81d3fc
Rafael Olivares <rafaelrolivares@gmail.com>
2023-01-11 15:22:48 -0300

    google btn

--- src/components/Form/GoogleButton.tsx
+++ src/components/Form/GoogleButton.tsx
@@ -8,19 +8,17 @@ type GoogleButtonProps = {
   route: RouteDetail;
 };
 
-const GoogleButton = ({ route }: GoogleButtonProps): ReactElement => {
-  return (
-    <div className="option-btn">
-      <a
-        className="google-link"
-        href={createGoogleMapsUrl(route)}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open on GoogleMaps
-      </a>
-    </div>
-  );
-};
+const GoogleButton = ({ route }: GoogleButtonProps): ReactElement => (
+  <div className="option-btn">
+    <a
+      className="google-link"
+      href={createGoogleMapsUrl(route)}
+      target="_blank"
+      rel="noreferrer"
+    >
+      Open on GoogleMaps
+    </a>
+  </div>
+);
 
 export default GoogleButton;
